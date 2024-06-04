825774daaed1c0f0cf59b213ebf95df8faf52d8f
GuTaeho <sbpdt1062@gmail.com>
2024-06-04 23:42:22 +0900

    Feat: extension 확장 추가

--- Package.swift
+++ Package.swift
@@ -12,12 +12,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyPackage",
-            targets: ["MyPackage"]),
+            targets: ["MyPackage"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyPackage")
+            name: "MyPackage"),
+        .target(
+            name: "MyExtension")
     ]
 )
---
+++ Sources/MyExtension/UIView+Color.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by 구태호 on 6/4/24.
+//
+
+import UIKit
+
+
+public extension UIView {
+    /// 뷰의 색상을 반환합니다.
+    /// - Returns: UIColor?
+    func printBackgroundColor() -> UIColor? {
+        self.backgroundColor
+    }
+}
